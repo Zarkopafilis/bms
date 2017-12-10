104afc0f0c64c0148ffa4b74be6d27966df688e0
Zarkopafilis <zarkopafilis@gmail.com>
2017-12-10 17:47:46 +0200

    finish, untested config code

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -5,6 +5,40 @@ void config_load(){
     byte flag = EEPROM.read(FLAG_ADDR);
     if(flag == CORRECT_FLAG){
         //read configuration
+        #if DEBUG
+            Serial.println("Configuration flag is set, loading data from EEPROM");
+        #endif
+        DCP_MODE = EEPROM.read(DCP_MODE_ADDR);
+        SLAVE_NUM = EEPROM.read(SLAVE_NUM_ADDR);
+        MAX_MEASURE_CYCLE_DURATION_MS = EEPROM.read(MAX_MEASURE_CYCLE_DURATION_MS_ADDR);
+        CELL_IGNORE_INDEX_START = EEPROM.read(CELL_IGNORE_INDEX_START_ADDR);
+        CELL_IGNORE_INDEX_END = EEPROM.read(CELL_IGNORE_INDEX_END_ADDR);
+        GPIO_IGNORE_INDEX_START = EEPROM.read(GPIO_IGNORE_INDEX_START_ADDR);
+        GPIO_IGNORE_INDEX_END = EEPROM.read(GPIO_IGNORE_INDEX_END_ADDR);
+
+        uint8_t lsb = EEPROM.read(TUT_ADDR);
+        uint8_t msb = EEPROM.read(TUT_ADDR + 1);
+        TUT = msb << 8 | lsb;
+
+        lsb = EEPROM.read(TOT_ADDR);
+        msb = EEPROM.read(TOT_ADDR + 1);
+        TOT = msb << 8 | lsb;
+
+        lsb = EEPROM.read(VUV_ADDR);
+        msb = EEPROM.read(VUV_ADDR + 1);
+        VUV = msb << 8 | lsb;
+
+        lsb = EEPROM.read(VOV_ADDR);
+        msb = EEPROM.read(VOV_ADDR);
+        VOV = msb << 8 | lsb;
+
+        for(uint8_t i = 0; i < 6; i++){
+            slave_cfg[i] = EEPROM.read(SLAVE_CONFIG_ADDR + i);
+        }
+    }else{
+        #if DEBUG
+            Serial.println("Configuration flag is not set, switching to defaults");
+        #endif
     }
 }
 
@@ -16,18 +50,55 @@ void config_persist(){
     EEPROM.write(CELL_IGNORE_INDEX_END_ADDR,CELL_IGNORE_INDEX_END);
     EEPROM.write(GPIO_IGNORE_INDEX_START_ADDR,GPIO_IGNORE_INDEX_START);
     EEPROM.write(GPIO_IGNORE_INDEX_END_ADDR,GPIO_IGNORE_INDEX_END);
-    EEPROM.write(TUT_ADDR,TUT);
-    EEPROM.write(TOT_ADDR,TOT);
+    EEPROM.write(TUT_ADDR, ((uint8_t)(TUT && 0xFF)));
+    EEPROM.write(TUT_ADDR + 1,((uint8_t) TUT >> 8));
 
-    EEPROM.write(VUV_ADDR, VUV && 0xFF);
-    EEPROM.write(VUV_ADDR + 1, VUV >> 8);
+    EEPROM.write(TOT_ADDR, ((uint8_t)(TOT && 0xFF)));
+    EEPROM.write(TOT_ADDR + 1,((uint8_t) TOT >> 8));
 
-    EEPROM.write(VOV_ADDR, VOV && 0xFF);
-    EEPROM.write(VOV_ADDR + 1, VOV >> 8);
+    EEPROM.write(VUV_ADDR, ((uint8_t)(VUV && 0xFF)));
+    EEPROM.write(VUV_ADDR + 1,((uint8_t) VUV >> 8));
+
+    EEPROM.write(VOV_ADDR, ((uint8_t)(VOV && 0xFF)));
+    EEPROM.write(VOV_ADDR + 1, ((uint8_t)VOV >> 8));
 
     for(uint8_t i = 0; i < 6; i++){
         EEPROM.write(SLAVE_CONFIG_ADDR + i, slave_cfg[i]);
     }
 
     EEPROM.write(FLAG_ADDR, CORRECT_FLAG);
+}
+
+void config_print(){
+    #if DEBUG
+        Serial.print("DCP_MODE -> ");
+        Serial.println(DCP_MODE);
+        Serial.print("SLAVE_NUM -> ");
+        Serial.println(SLAVE_NUM);
+        Serial.print("MAX_MEASURE_CYCLE_DURATION_MS -> ");
+        Serial.println(MAX_MEASURE_CYCLE_DURATION_MS);
+        Serial.print("CELL_IGNORE_INDEX_START -> ");
+        Serial.println(CELL_IGNORE_INDEX_START);
+        Serial.print("CELL_IGNORE_INDEX_END -> ");
+        Serial.println(CELL_IGNORE_INDEX_END);
+        Serial.print("GPIO_IGNORE_INDEX_START -> ");
+        Serial.println(GPIO_IGNORE_INDEX_START);
+        Serial.print("GPIO_IGNORE_INDEX_END -> ");
+        Serial.println(GPIO_IGNORE_INDEX_END_ADDR);
+        Serial.print("TUT -> ");
+        Serial.println(TUT);
+        Serial.print("TOT -> ");
+        Serial.println(TOT);
+        Serial.print("VUV -> ");
+        Serial.println(VUV);
+        Serial.print("VOV -> ");
+        Serial.println(VOV);
+
+        for(uint8_t i = 0; i < 6; i++){
+            Serial.print("slave_cfg[");
+            Serial.print(i);
+            Serial.print("] -> ");
+            Serial.println(slave_cfg[i]);
+        }
+    #endif
 }
--- src/main/config.h
+++ src/main/config.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <SPI.h>
+#include "LTC68042.h"
 
 #define DEBUG 1
 #define DEBUG_PEC 1
@@ -29,12 +30,12 @@ uint8_t slave_cfg[6] =
 
 //Cell discharge permitted
 //Disabled = 0 | Enabled = 1
-uint8_t DCP_MODE = DCP_DISABLED
+uint8_t DCP_MODE = DCP_DISABLED;
 
 //Number of LTC6811-2 Multicell battery monitors
-uint8_t SLAVE_NUM = 1
+uint8_t SLAVE_NUM = 1;
 
-uint8_t MAX_MEASURE_CYCLE_DURATION_MS = 500
+uint16_t MAX_MEASURE_CYCLE_DURATION_MS = 500;
 
 //Cells with index > CELL_IGNORE_INDEX will be ignored from measurements.
 //This is useful for cases where your cells on a module 
@@ -44,22 +45,22 @@ uint8_t MAX_MEASURE_CYCLE_DURATION_MS = 500
 //are going to be ignored both from the open wire checks and
 //the measurements
 //Set to 12(Max Cells) in order to measure all
-uint8_t CELL_IGNORE_INDEX_START = 2
-uint8_t CELL_IGNORE_INDEX_END = 12
+uint8_t CELL_IGNORE_INDEX_START = 2;
+uint8_t CELL_IGNORE_INDEX_END = 12;
 
 //Same with CELL_IGNORE_INDEX, but for the GPIOs where the temperature
 //sensors (or thermistors) are wired in. The monitors have 5 GPIOs
 //Set to 5(Max GPIOs) in order to measure all
-uint8_t GPIO_IGNORE_INDEX_START = 0
-uint8_t GPIO_IGNORE_INDEX_END = 5
+uint8_t GPIO_IGNORE_INDEX_START = 0;
+uint8_t GPIO_IGNORE_INDEX_END = 5;
 
 //Temperature Voltage Read for Undertemping and Overtemping
-uint8_t TUT -10
-uint8_t TOT 100
+uint16_t TUT = 5;
+uint16_t TOT = 100;
 
 //Voltage for Undervolting and Overvolting
-uint16_t VUV = 2
-uint16_t VOV = 5
+uint16_t VUV = 2;
+uint16_t VOV = 5;
 
 //Teensy 3.1 & 3.2 has got 2048 bytes available
 #define FLAG_ADDR 0
@@ -67,23 +68,27 @@ uint16_t VOV = 5
 
 #define DCP_MODE_ADDR 1
 #define SLAVE_NUM_ADDR 2
+//Note MAX_MEASURE_CYCLE_DURATION_MS IS uint16 | 2 bytes 
 #define MAX_MEASURE_CYCLE_DURATION_MS_ADDR 3
 #define CELL_IGNORE_INDEX_START_ADDR 4
 #define CELL_IGNORE_INDEX_END_ADDR 5
 #define GPIO_IGNORE_INDEX_START_ADDR 6
 #define GPIO_IGNORE_INDEX_END_ADDR 7
+//Note TUT, TOT, VUV & VOV are uint16 | 2 bytes each
 #define TUT_ADDR 8
-#define TOT_ADDR 9
-//Note VUV & VOV are uint16 | 2 bytes each
-#define VUV_ADDR 10
-#define VOV_ADDR 12
+#define TOT_ADDR 10
+#define VUV_ADDR 12
+#define VOV_ADDR 14
 //uint8_t slave_cfg[6] | 6 bytes
-#define SLAVE_CONFIG_ADDR 14
+#define SLAVE_CONFIG_ADDR 16
 
 //Loads config from EEPROM if persist flag is correct
 void config_load();
 
 //Persists current config to EEPROM and sets persist flag 
 void config_persist();
+
+//Writes the whole config to serial if DEBUG is 1
+void config_print();
 
 #endif //CONFIG_H
